d8a42095d70f8ca9d5a8d6a8e67a4da680d52562
masqomar <mqomarudin@icloud.com>
2025-10-10 16:11:02 +0700

    feat: routing file err

--- src/template/OtpTemplate.ts
+++ src/template/OtpTemplate.ts
@@ -3,7 +3,7 @@ import fs from 'fs'
 import { CONFIG } from '@/config'
 
 export function templateOtpHtml(data: { otp: string }): string {
-  const pagePath = path.resolve(process.cwd(), './src/Template/HTML/otp.html')
+  const pagePath = path.resolve(process.cwd(), './src/template/HTML/otp.html')
   const template = fs.readFileSync(pagePath, 'utf8')
 
   console.log('TemplateHtml', data)
